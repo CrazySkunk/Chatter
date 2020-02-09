642a7ebf470d8679ab2d2cb64d1d43b532eb0e6e
JAMES OMONDI <jamesomondi1418@gmail.com>
2020-02-09 23:10:20 +0300

    updated files

--- app/src/main/java/ke/ac/akademiks/chatter/message_area.java
+++ app/src/main/java/ke/ac/akademiks/chatter/message_area.java
@@ -2,13 +2,37 @@ package ke.ac.akademiks.chatter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class message_area extends AppCompatActivity {
+    EditText message;
+    ImageView send;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_area);
+
+        this.message = findViewById(R.id.messageArea);
+        this.send = findViewById(R.id.sendButton);
+        text = message.getText().toString();
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage(text);
+            }
+        });
+
+    }
+
+    public void sendMessage(String text) {
+        Intent send = new Intent(this,chat.class);
+        send.putExtra(text,0);
+        startActivity(send);
     }
 }
